b2de5ba8f4cc4258a5761f14fa8348e1bb138da1
Leonides Muguercia Tellez <leonimuguercia5@gmail.com>
2024-01-23 18:27:13 +0100

    Encriptar contraseña, hacer login, verificar contraseña y email

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,14 +2,20 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { AuthService } from './auth.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { LoginDto } from './dto/login.dto';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(private readonly authService: AuthService) { }
 
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.authService.create(createUserDto);
+  }
+
+  @Post('/login')
+  login(@Body() loginDto: LoginDto) {
+    return this.authService.login(loginDto)
   }
 
   @Get()
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,31 +1,59 @@
-import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, UnauthorizedException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { User } from './entities/user.entity';
 import { Model } from 'mongoose';
+import * as bcryptjs from 'bcryptjs'
+import { LoginDto } from './dto/login.dto';
 
 @Injectable()
 export class AuthService {
   constructor(@InjectModel(User.name) private userModel: Model<User>) { }
 
   async create(createUserDto: CreateUserDto): Promise<User> {
-    // 1. Encriptar contrase;a
-    // 2. Guardar el usuario
-    // 3. Generar el JWT
-
-
     try {
-      const newUser = new this.userModel(createUserDto);
-      return await newUser.save()
+      const { password, ...userData } = createUserDto;
+      // 1. Encriptar contrase;a
+      const newUser = new this.userModel({
+        password: bcryptjs.hashSync(password, 10),
+        ...userData
+      });
+      await newUser.save()
+      const { password: _, ...user } = newUser.toJSON()
+      return user
+      // 2. Guardar el usuario
+      // 3. Generar el JWT
     } catch (error) {
       console.log(error.code)
-      if(error.code == 11000){
+      if (error.code == 11000) {
         throw new BadRequestException(`${createUserDto.email} already exists!`)
       }
       throw new InternalServerErrorException(`Something terrible happen`)
+    }
+  }
+
+  async login(loginDto: LoginDto) {
+    const {email, password} = loginDto
+    const user = await this.userModel.findOne({email})
+    if(!user) {
+      throw new UnauthorizedException('Not valid credential -> email')
+    }
+    if(!bcryptjs.compareSync(password, user.password)) {
+      throw new UnauthorizedException('Not valid credential -> password')
 
     }
+    const { password:_, ...rest } =user.toJSON()
+    console.log(user.toJSON())
+
+    return {
+      ...rest,
+      token: 'ABC-123'
+    }
+    /**
+     * User {_id, name, email, roles}
+     * Token => 
+     */
   }
 
   findAll() {
